7e65bc397b2bb59a58dc52e1263bfe168e61eb02
euberdeveloper <euberdeveloper@gmail.com>
2021-11-03 14:53:26 +0100

    feat(Test.vue): add first test with stl model
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -6,6 +6,9 @@
 import { defineComponent } from "vue";
 
 import * as THREE from "three";
+import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Stats from "three/examples/jsm/libs/stats.module";
 
 export default defineComponent({
   name: "Test",
@@ -13,30 +16,85 @@ export default defineComponent({
     const container = this.$refs.container as HTMLDivElement;
 
     const scene = new THREE.Scene();
+    scene.add(new THREE.AxesHelper(5));
+
+    const light = new THREE.SpotLight();
+    light.position.set(40, 0, 100);
+    light.power = 1000;
+    scene.add(light);
+
+    const light1 = new THREE.SpotLight();
+    light1.position.set(-20, -20, -20);
+    light1.power = 1000;
+    scene.add(light1);
+
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(100, 0, 100);
 
     const renderer = new THREE.WebGLRenderer();
+    renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
 
-    camera.position.z = 2;
+    // const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"]);
+    // envTexture.mapping = THREE.CubeReflectionMapping;
 
-    const animate = function () {
+    const material = new THREE.MeshPhysicalMaterial({
+      color: 0xb2ffc8,
+      // envMap: envTexture,
+      metalness: 0.7,
+      roughness: 0.1,
+      opacity: 1.0,
+      transparent: false,
+      transmission: 0.99,
+      clearcoat: 1.0,
+      clearcoatRoughness: 0.25,
+    });
+
+    const loader = new STLLoader();
+    loader.load(
+      '/models/levetta_test.stl',
+      function (geometry) {
+        const mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    window.addEventListener("resize", onWindowResize, false);
+    function onWindowResize() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      render();
+    }
+
+    const stats = Stats();
+    container.appendChild(stats.dom);
+
+    function animate() {
       requestAnimationFrame(animate);
 
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      controls.update();
 
+      render();
+
+      stats.update();
+    }
+
+    function render() {
       renderer.render(scene, camera);
-    };
+    }
 
     animate();
-    setInterval(() => camera.position.z += 0.01, 20);
   },
 });
 </script>
