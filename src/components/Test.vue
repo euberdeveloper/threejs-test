1248c6222a56fed95c82f4a94ed4a0a967008f1e
euberdeveloper <euberdeveloper@gmail.com>
2021-11-03 14:30:10 +0100

    feat(Test.vue): add first test with threejs
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,16 +1,42 @@
 <template>
-  <div class="test">
-    
-  </div>
+  <div class="test" ref="container"></div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
-import THREE from 'three';
+import * as THREE from "three";
 
 export default defineComponent({
-  name: 'Test'
+  name: "Test",
+  mounted() {
+    const container = this.$refs.container as HTMLDivElement;
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    const animate = function () {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
+  },
 });
 </script>
 
