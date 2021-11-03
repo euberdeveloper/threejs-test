3c9e541761703bd4a93c46910fe83037e129b00c
euberdeveloper <euberdeveloper@gmail.com>
2021-11-03 14:25:54 +0100

    feat: add stl test and three deps
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <test />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+import Test from '@/components/Test.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Test
   }
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="test">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import THREE from 'three';
+
+export default defineComponent({
+  name: 'Test'
+});
+</script>
+
+<style scoped lang="scss">
+</style>
